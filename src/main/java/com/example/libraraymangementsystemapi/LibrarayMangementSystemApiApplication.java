a44182a3968d224a33376af68934613e1478d548
Salah Ashraf <salahashraf924@gmail.com>
2025-02-17 20:35:37 +0200

    feat: project structure

---
+++ src/main/java/com/example/libraraymangementsystemapi/LibrarayMangementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraraymangementsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarayMangementSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarayMangementSystemApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/libraraymangementsystemapi/LibrarayMangementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraraymangementsystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarayMangementSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
