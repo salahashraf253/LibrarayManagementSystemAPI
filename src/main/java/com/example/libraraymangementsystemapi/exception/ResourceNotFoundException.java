c241c2985f47ba0a60ba72be78aea45a20bfabab
Salah Ashraf <salahashraf924@gmail.com>
2025-03-07 14:25:25 +0200

    feat: add resource not found expection

--- src/main/java/com/example/libraraymangementsystemapi/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/libraraymangementsystemapi/exception/ResourceNotFoundException.java
@@ -1,2 +1,7 @@
-package com.example.libraraymangementsystemapi.exception;public class ResourceNotFoundException {
+package com.example.libraraymangementsystemapi.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
