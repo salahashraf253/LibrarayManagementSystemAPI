a7e18401c86d54c88189b6233423f2f9250f1306
Salah Ashraf <salahashraf924@gmail.com>
2025-03-19 16:41:30 +0200

    feat: create custom exception

---
+++ src/main/java/com/example/libraraymangementsystemapi/exception/InvalidRoleException.java
@@ -0,0 +1,7 @@
+package com.example.libraraymangementsystemapi.exception;
+
+public class InvalidRoleException extends RuntimeException {
+    public InvalidRoleException(String message) {
+        super(message);
+    }
+}
