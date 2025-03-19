d3b061e6bf1095e1e7a79aed6eb324602269e250
Salah Ashraf <salahashraf924@gmail.com>
2025-03-19 16:41:44 +0200

    fix: field injection

--- src/main/java/com/example/libraraymangementsystemapi/Mappers/UserMapper.java
+++ src/main/java/com/example/libraraymangementsystemapi/Mappers/UserMapper.java
@@ -5,12 +5,13 @@ import com.example.libraraymangementsystemapi.factories.UserFactoryProvider;
 import com.example.libraraymangementsystemapi.dto.request.RegistrationRequest;
 import com.example.libraraymangementsystemapi.dto.response.RegistrationResponse;
 import com.example.libraraymangementsystemapi.entity.User;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class UserMapper {
-    @Autowired
     private UserFactoryProvider userFactoryProvider;
 
     public RegistrationResponse sourceToDestination(User user) {
