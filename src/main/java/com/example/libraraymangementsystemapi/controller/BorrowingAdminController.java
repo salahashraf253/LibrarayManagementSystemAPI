867be97ab7334322fcf76b752b5c4e06471fbda9
Salah Ashraf <salahashraf924@gmail.com>
2025-03-19 16:21:53 +0200

    fix: Remove this unused import

--- src/main/java/com/example/libraraymangementsystemapi/controller/BorrowingAdminController.java
+++ src/main/java/com/example/libraraymangementsystemapi/controller/BorrowingAdminController.java
@@ -7,7 +7,6 @@ import com.example.libraraymangementsystemapi.service.BorrowingService;
 import com.example.libraraymangementsystemapi.util.ExtraDataUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
