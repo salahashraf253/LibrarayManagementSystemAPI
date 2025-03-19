1570de31db9e265f2cdf136776da6652792040db
Salah Ashraf <salahashraf924@gmail.com>
2025-03-19 16:01:59 +0200

    fix: remove filed injection

--- src/main/java/com/example/libraraymangementsystemapi/Factories/AdminFactory.java
+++ src/main/java/com/example/libraraymangementsystemapi/Factories/AdminFactory.java
@@ -3,13 +3,14 @@ package com.example.libraraymangementsystemapi.Factories;
 import com.example.libraraymangementsystemapi.dto.request.RegistrationRequest;
 import com.example.libraraymangementsystemapi.entity.Admin;
 import com.example.libraraymangementsystemapi.entity.User;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class AdminFactory implements UserFactory{
-    @Autowired
     private PasswordEncoder passwordEncoder;
     @Override
     public User createUser(RegistrationRequest registrationRequest) {
